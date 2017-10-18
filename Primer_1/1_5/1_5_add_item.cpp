e761f057a0110ff8cebb4643c4f44eb1e6240fbe
TwistedW <2359375192@qq.com>
2017-10-18 20:33:41 +0800

    updating

--- Primer_1/1_5/1_5_add_item.cpp
+++ Primer_1/1_5/1_5_add_item.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
-#include"Sales_item.h"
+//#include"Sales_item.h"
 
 using namespace std;
 
-int main()
+int main2()
 {
-    Sales_item item1;
-    Sales_item item2;
-    Sales_item item3;
-    std::cin>>item1>>item2>>item3;
-    std::cout<<item1+item2+item3<<std::endl;
+    //Sales_item item1;
+    //Sales_item item2;
+    //Sales_item item3;
+    //std::cin>>item1>>item2>>item3;
+    //std::cout<<item1+item2+item3<<std::endl;
     return 0;
 }
