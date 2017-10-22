2641c3d61f367470283f9f243275a9a5b4418101
TwistedW <2359375192@qq.com>
2017-10-22 23:05:53 +0800

    updating

---
+++ Primer_2/2_2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int i=42;
+int main()
+{
+    int input_value=3.14, i=100;
+    int j=i;
+    double Double=3.14;
+    //int 1_or_2=1;
+    std::cout<<input_value<<"\t"<<Double<<" "<<j<<std::endl;
+    return 0;
+}
