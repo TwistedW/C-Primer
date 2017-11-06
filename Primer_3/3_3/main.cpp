e168f04d8c6093372adaa35fff304fb0a60b4e55
TwistedW <2359375192@qq.com>
2017-11-06 09:01:49 +0800

    updating

--- Primer_3/3_3/main.cpp
+++ Primer_3/3_3/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include<vector>
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    vector<int> v1;
+    for(int i=0; i<=100; ++i)
+        v1.push_back(i);
+    cout<<"v1 size:"<<v1.size()<<endl;
+    for(decltype(v1.size()) j=0; j<v1.size(); j++)
+           cout<<v1[j]<<endl;
     return 0;
 }
