fba7ad49555e16a97d2f476641d137283db7622d
TwistedW <2359375192@qq.com>
2017-11-07 20:21:48 +0800

    updating

---
+++ Primer_3/3_6/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    constexpr size_t rowCnt=3, colCnt=4;
+    size_t cnt=0;
+    int a[rowCnt][colCnt];
+    for(size_t i=0; i!=rowCnt; ++i)
+        for(size_t j=0; j!=colCnt; ++j)
+        {
+            a[i][j]=i*colCnt + j;
+            cout << a[i][j]<< endl;
+        }
+    for(auto &row:a)
+        for(auto &col:row)
+    {
+        col = cnt;
+        ++cnt;
+        cout << col << endl;
+    }
+    return 0;
+}
